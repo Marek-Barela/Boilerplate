37eae69908c5e3094d51e1a8df01bb24f4eea568
Marek Bareła <marek.barela@wp.pl>
2019-03-10 22:09:04 +0100

    Sample feature update

--- features/timer/reducer.js
+++ features/timer/reducer.js
@@ -10,12 +10,6 @@ export const exampleInitialState = {
 
 function reducer(state = exampleInitialState, action) {
   switch (action.type) {
-    case actionTypes.FAILURE:
-      return {
-        ...state,
-        ...{ error: action.error }
-      }
-
     case actionTypes.INCREMENT:
       return {
         ...state,
@@ -33,19 +27,6 @@ function reducer(state = exampleInitialState, action) {
         ...state,
         ...{ count: exampleInitialState.count }
       }
-
-    case actionTypes.LOAD_DATA_SUCCESS:
-      return {
-        ...state,
-        ...{ placeholderData: action.data }
-      }
-
-    case actionTypes.TICK_CLOCK:
-      return {
-        ...state,
-        ...{ lastUpdate: action.ts, light: !!action.light }
-      }
-
     default:
       return state
   }
--- features/timer/saga.js
+++ features/timer/saga.js
@@ -1,34 +1,16 @@
 /* global fetch */
 
-import { all, call, delay, put, take, takeLatest } from 'redux-saga/effects'
-import es6promise from 'es6-promise'
-import 'isomorphic-unfetch'
-
-import { actionTypes, failure, loadDataSuccess, tickClock } from './actions'
-
+import { all, call } from 'redux-saga/effects';
+import es6promise from 'es6-promise';
+import 'isomorphic-unfetch';
 es6promise.polyfill()
 
 function* runClockSaga() {
-  yield take(actionTypes.START_CLOCK)
-  while (true) {
-    yield put(tickClock(false))
-    yield delay(1000)
-  }
-}
-
-function* loadDataSaga() {
-  try {
-    const res = yield fetch('https://jsonplaceholder.typicode.com/users')
-    const data = yield res.json()
-    yield put(loadDataSuccess(data))
-  } catch (err) {
-    yield put(failure(err))
-  }
+  console.log("Saga Works!")
 }
 
 export default function* () {
   yield all([
     call(runClockSaga),
-    takeLatest(actionTypes.LOAD_DATA, loadDataSaga)
   ])
 }
